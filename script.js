927a75bb3795a3b234e96ecc34e0f6d0871614fd
antonmaenpaa <antonmaenpaa@gmail.com>
2020-11-10 13:48:41 +0100

    add html,css and js file

---
+++ script.js
@@ -0,0 +1,5 @@
+// 1.Skapa en ny sida där du placerar en färglagd cirkel på sidan 
+
+// 2.Lägg sedan till en eventlyssnare på document som gör att cirklen flyttar sig till där man klickar på piltangenterna. 
+
+// 3.Kan du få till så att den blir centrerad?
